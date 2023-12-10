2f474e1dd4081e2f6309e79b6ba037a3b803dfe7
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2023-12-10 18:32:20 +0100

    tweak

--- day10/day10.hpp
+++ day10/day10.hpp
@@ -25,7 +25,6 @@ private:
         int x, y;
         char nodeType;
         bool isPartOfLoop = false;
-        bool isInsideLoop = false;
         std::shared_ptr<Node> pipeTo;
         std::shared_ptr<Node> pipeFrom;
     };
@@ -68,126 +67,107 @@ private:
 
     void PopulateNodesMap() {
         nodes.clear();
-        for (int i = 0; i < lines.size(); i++) {
-            for (int j = 0; j < lines[i].size(); j++) {
-                auto letter = lines[i][j];
+        for (int y = 0; y < lines.size(); y++) {
+            for (int x = 0; x < lines[y].size(); x++) {
+                auto letter = lines[y][x];
                 auto newNode = std::make_shared<Node>();
                 newNode->nodeType = letter;
-                newNode->y = j;
-                newNode->x = i;
-                nodes[i][j] = newNode;
+                newNode->y = y;
+                newNode->x = x;
+                nodes[y][x] = newNode;
                 allNodes.push_back(newNode);
-                PopulateNodeLinks(i, j, newNode);
+                PopulateNodeLinks(newNode);
             }
         }
     }
 
-    void PopulateNodeLinks(int i, int j, const std::shared_ptr<Node>& thisNode) {
-        bool findAbove = false;
-        bool findLeft = false;
+    void PopulateNodeLinks(const std::shared_ptr<Node>& thisNode) {
+        bool supportUpwardsConnection = false;
+        bool supportLeftConnection = false;
         switch (thisNode->nodeType) {
             case '-':
-                findLeft = true;
+                supportLeftConnection = true;
                 break;
             case '|':
-                findAbove = true;
+                supportUpwardsConnection = true;
                 break;
             case 'J':
-                findLeft = true;
-                findAbove = true;
+                supportLeftConnection = true;
+                supportUpwardsConnection = true;
                 break;
             case 'L':
-                findAbove = true;
+                supportUpwardsConnection = true;
                 break;
             case 'F':
                 break;
             case '7':
-                findLeft = true;
+                supportLeftConnection = true;
                 break;
             case 'S':
-                findLeft = true;
-                findAbove = true;
+                supportLeftConnection = true;
+                supportUpwardsConnection = true;
                 startNode = thisNode;
                 break;
             default:
                 break;
-                // | is a vertical pipe connecting north and south.
-                //- is a horizontal pipe connecting east and west.
-                //L is a 90-degree bend connecting north and east.
-                //J is a 90-degree bend connecting north and west.
-                //7 is a 90-degree bend connecting south and west.
-                //F is a 90-degree bend connecting south and east.
-                //. is ground; there is no pipe in this tile.
-                //S is the starting position of the animal; there is a pipe on this
         }
 
-        if (findAbove) {
-            auto nodeItAbove = GetValueIfExists(nodes, i - 1, j);
-            if (nodeItAbove != nullptr) {
-                bool shouldConnect = false;
-                switch (nodeItAbove->nodeType) {
+        if (supportUpwardsConnection) {
+            auto nodeToConnect = GetValueIfExists(nodes, thisNode->y - 1, thisNode->x);
+            if (nodeToConnect != nullptr) {
+                switch (nodeToConnect->nodeType) {
                     case '|':
                     case 'S':
                     case '7':
                     case 'F':
-                        shouldConnect = true;
+                        ConnectNodes(thisNode, nodeToConnect);
                         break;
                     default:
                         break;
                 }
-                if (shouldConnect) {
-                    if (nodeItAbove->pipeTo == nullptr) {
-                        nodeItAbove->pipeTo = thisNode;
-                    } else {
-                        nodeItAbove->pipeFrom = thisNode;
-                    }
-                    if (thisNode->pipeFrom == nullptr) {
-                        thisNode->pipeFrom = nodeItAbove;
-                    } else {
-                        thisNode->pipeTo = nodeItAbove;
-                    }
-                }
             }
         }
-        if (findLeft) {
-            auto nodeItLeft = GetValueIfExists(nodes, i, j-1);
-            if (nodeItLeft != nullptr) {
-                bool shouldConnect = false;
-                switch (nodeItLeft->nodeType) {
+
+        if (supportLeftConnection) {
+            auto nodeToConnect = GetValueIfExists(nodes, thisNode->y, thisNode->x - 1);
+            if (nodeToConnect != nullptr) {
+                switch (nodeToConnect->nodeType) {
                     case '-':
                     case 'F':
                     case 'L':
                     case 'S':
-                        shouldConnect = true;
+                        ConnectNodes(thisNode, nodeToConnect);
                         break;
                     default:
                         break;
-                }
-                if (shouldConnect) {
-                    if (nodeItLeft->pipeTo == nullptr) {
-                        nodeItLeft->pipeTo = thisNode;
-                    } else {
-                        nodeItLeft->pipeFrom = thisNode;
-                    }
-                    if (thisNode->pipeFrom == nullptr) {
-                        thisNode->pipeFrom = nodeItLeft;
-                    } else {
-                        thisNode->pipeTo = nodeItLeft;
-                    }
                 }
             }
         }
     }
 
+    static void ConnectNodes(const std::shared_ptr<Node>& n1, const std::shared_ptr<Node>& n2) {
+        if (n1 == nullptr || n2 == nullptr) return;
+        if (n2->pipeTo == nullptr) {
+            n2->pipeTo = n1;
+        } else {
+            n2->pipeFrom = n1;
+        }
+        if (n1->pipeFrom == nullptr) {
+            n1->pipeFrom = n2;
+        } else {
+            n1->pipeTo = n2;
+        }
+    }
+
     static std::shared_ptr<Node> GetValueIfExists(const Nodes& map,
-                                                 const int& key1, const int& key2) {
-        if (key1 < 0 || key2 < 0) {
+                                                 const int& y, const int& x) {
+        if (x < 0 || y < 0) {
             return nullptr;
         }
-        auto it = map.find(key1);
+        auto it = map.find(y);
         if (it != map.end()) {
             const auto& innerMap = it->second;
-            const auto innerIt = innerMap.find(key2);
+            const auto innerIt = innerMap.find(x);
             if (innerIt != innerMap.end()) {
                 return innerIt->second;
             }
