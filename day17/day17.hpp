6dfd218f9f57f299cca16b8869d4ae634f2e9e50
Mathias Hatlestad <mathias.hatlestad@bidbax.no>
2023-12-19 09:35:51 +0100

    fix day 17

--- day17/day17.hpp
+++ day17/day17.hpp
@@ -16,21 +16,23 @@ public:
     Day17() {
         std::string name = "day17";
         std::cout << name << std::endl;
-        Utilities::ReadFile(name + "/example.txt", lines);
+        Utilities::ReadFile(name + "/input.txt", lines);
         Solve();
     }
 private:
     std::vector<std::string> lines;
 
     enum Direction {
-        Up, Down, Left, Right
+        Up = 1, Down = 2, Left = 3, Right = 4
     };
 
     struct State {
-        int totalHeat;
+        int totalHeatLoss;
         std::pair<int, int> pos;
         std::pair<int, int> prevStatePos;
-        std::vector<Direction> directions;
+        Direction direction;
+        int stepsInDir;
+        std::vector<std::pair<int, int>> prevPositions;
     };
 
     typedef std::vector<std::vector<int>> NodeMap;
@@ -62,7 +64,7 @@ private:
         }
     }
 
-    Direction GetDir(const std::pair<int, int>& newPos, const std::pair<int, int>& fromPos) {
+    static Direction GetDir(const std::pair<int, int>& newPos, const std::pair<int, int>& fromPos) {
         if (newPos.first > fromPos.first) return Direction::Down;
         if (newPos.first < fromPos.first) return Direction::Up;
         if (newPos.second > fromPos.second) return Direction::Right;
@@ -70,19 +72,21 @@ private:
         throw;
     };
 
-    bool isValid(const std::pair<int, int>& newPos, const std::pair<int, int>& currPos, const std::pair<int, int>& prevState, const std::vector<Direction>& directions) {
-        if (newPos.first == prevState.first && newPos.second == prevState.second)
-            return false;
+    bool isValid(const std::pair<int, int>& newPos, const std::pair<int, int>& currPos, const std::pair<int, int>& prevPos, const Direction& prevDir, int stepsInDir, const std::vector<std::pair<int, int>> allPos) {
+        if (newPos == prevPos) return false;
+        if (std::find(allPos.begin(), allPos.end(), std::make_pair(int(nodeMap.size() - 1), int(nodeMap[0].size()-1))) != allPos.end()) return false;
+        if (std::find(allPos.begin(), allPos.end(), newPos) != allPos.end()) return false;
 
         auto newDir = GetDir(newPos, currPos);
-
-        auto dirSize = directions.size();
-        if (dirSize >= 3) {
-            if (directions[dirSize-1] == newDir
-                && directions[dirSize-2] == newDir
-                && directions[dirSize-3] == newDir) {
+        if (newDir == prevDir) {
+            if (stepsInDir >= 10) {
                 return false;
             }
+        } else if (allPos.size() < 3) {
+            if (newDir != prevDir)
+                return false;
+        } else if (stepsInDir < 4 && allPos.size() >= 4) {
+            return false;
         }
 
         return (newPos.first >= 0) && (newPos.first < nodeMap.size()) && (newPos.second >= 0) && (newPos.second < nodeMap[0].size());
@@ -91,17 +95,19 @@ private:
     class Compare {
     public:
         bool operator() (const State &a, const State& b) {
-            return a.totalHeat < b.totalHeat;
+            return a.totalHeatLoss > b.totalHeatLoss;
         }
     };
 
     int calculateShortestWithDijkstra(int startRow, int startCol, int endRow, int endCol) {
-        std::vector<std::vector<int>> minHeatMap(nodeMap.size(), std::vector<int>(nodeMap[0].size(), INT_MAX - 10000));
+        std::vector<std::vector<std::vector<std::vector<int>>>> minHeatMap(nodeMap.size(), std::vector(nodeMap[0].size(), std::vector(5, std::vector(11, INT_MAX - 10000))));
 
         std::priority_queue<State, std::vector<State>, Compare> pq;
 
-        pq.push(State {0, {startRow, startCol}, {-10, -10}, {}});
-        minHeatMap[startRow][startCol] = 0;
+        pq.push(State {0, {startRow, startCol}, {startRow, startCol}, Down, 0, {{startRow, startCol}}});
+        pq.push(State {0, {startRow, startCol}, {startRow, startCol}, Right, 0, {{startRow, startCol}}});
+
+        minHeatMap[startRow][startCol][0][0] = 0;
 
         std::vector<std::pair<int, int>> explore {
                 {-1, 0},
@@ -110,23 +116,46 @@ private:
                 {0, -1},
         };
 
+        State endState;
+        endState.totalHeatLoss = INT32_MAX;
+
         while (!pq.empty()) {
             auto currentState = pq.top();
             pq.pop();
 
+            if (currentState.pos.first == endRow
+            && currentState.pos.second == endCol
+            && endState.totalHeatLoss > currentState.totalHeatLoss
+            && currentState.stepsInDir >= 4) {
+                endState = currentState;
+                break;
+            }
+
             for (auto& exp : explore) {
                 auto newPos = std::make_pair(currentState.pos.first + exp.first, currentState.pos.second + exp.second);
-                if (!isValid(newPos, currentState.pos, currentState.prevStatePos, currentState.directions)) continue;
-                int totalHeat = minHeatMap[currentState.pos.first][currentState.pos.second] + nodeMap[newPos.first][newPos.second];
-                auto existingMinHeat = minHeatMap[newPos.first][newPos.second];
+                if (!isValid(newPos, currentState.pos, currentState.prevStatePos, currentState.direction, currentState.stepsInDir, currentState.prevPositions)) {
+                    continue;
+                }
+
+                auto newDir = GetDir(newPos, currentState.pos);
+                auto stepsInDir = newDir == currentState.direction ? currentState.stepsInDir + 1 : 1;
+                int totalHeat = currentState.totalHeatLoss + nodeMap[newPos.first][newPos.second];
+                auto existingMinHeat = minHeatMap[newPos.first][newPos.second][newDir][stepsInDir];
+
+                if (totalHeat > endState.totalHeatLoss) {
+                    continue;
+                }
+
                 if (totalHeat < existingMinHeat) {
-                    minHeatMap[newPos.first][newPos.second] = totalHeat;
+                    minHeatMap[newPos.first][newPos.second][newDir][stepsInDir] = totalHeat;
                     State newState;
                     newState.pos = newPos;
-                    newState.totalHeat = totalHeat;
+                    newState.totalHeatLoss = totalHeat;
                     newState.prevStatePos = currentState.pos;
-                    newState.directions = currentState.directions;
-                    newState.directions.push_back(GetDir(newPos, currentState.pos));
+                    newState.direction = newDir;
+                    newState.stepsInDir = stepsInDir;
+                    newState.prevPositions = currentState.prevPositions;
+                    newState.prevPositions.push_back(newPos);
                     pq.push(newState);
                 }
             }
@@ -134,10 +163,15 @@ private:
 
         for (int i = 0; i < nodeMap.size(); ++i) {
             for (int j = 0; j < nodeMap[0].size(); ++j) {
-                std::cout << minHeatMap[i][j] << " ";
+                if (std::find(endState.prevPositions.begin(), endState.prevPositions.end(), std::make_pair(i, j)) != endState.prevPositions.end()) {
+                    std::cout << "'" << nodeMap[i][j] << "\t\t";
+                } else {
+                    std::cout << nodeMap[i][j] << "\t\t";
+                }
             }
             std::cout << std::endl;
         }
-        return minHeatMap[endRow][endCol];
+
+        return endState.totalHeatLoss;
     }
 };
